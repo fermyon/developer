a6c89ce395639d0a88d0ac88e1971596ec9fedf6
karthik Ganeshram <karthik.ganeshram@fermyon.com>
2022-10-06 05:02:23 -0700

    feat: add support for anchoring headings
    Signed-off-by: karthik Ganeshram <karthik.ganeshram@fermyon.com>

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,35 @@
 var header = new Headroom(document.querySelector("#topbar"), {
-    tolerance: 5,
-    offset : 80
+  tolerance: 5,
+  offset: 80
 });
 
 var blogAd = new Headroom(document.querySelector("#blogSlogan"), {
   tolerance: 5,
-  offset : 300
+  offset: 300
 });
 
-document.querySelectorAll('.modal-button').forEach(function(el) {
-  el.addEventListener('click', function() {
+document.querySelectorAll('.modal-button').forEach(function (el) {
+  el.addEventListener('click', function () {
     var target = document.querySelector(el.getAttribute('data-target'));
-    
+
     target.classList.add('is-active');
-    target.querySelector('.modal-close').addEventListener('click',   function() {
-        target.classList.remove('is-active');
+    target.querySelector('.modal-close').addEventListener('click', function () {
+      target.classList.remove('is-active');
     });
-    target.querySelector('.modal-background').addEventListener('click',   function() {
-        target.classList.remove('is-active');
-     });
+    target.querySelector('.modal-background').addEventListener('click', function () {
+      target.classList.remove('is-active');
+    });
   });
 });
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
   // Initialize after the DOM.
   (function () {
     var burger = document.querySelector('.burger');
     var menu = document.querySelector('#' + burger.dataset.target);
     burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
     });
   })();
 
@@ -38,9 +38,18 @@ document.addEventListener("DOMContentLoaded", function(){
   if (navigator && navigator.clipboard) {
     addCopyButtons(navigator.clipboard);
   }
+  addAnchorLinks()
+
+  if (window.location.hash.length > 0) {
+    setTimeout(function () {
+      document.querySelector('a[href="' + window.location.hash + '"]').click()
+    }, 150)
+    header.unpin()
+  }
+
 });
 
-if (document.body.contains(document.getElementById('blogSlogan'))){
+if (document.body.contains(document.getElementById('blogSlogan'))) {
   blogAd.init();
 };
 
@@ -76,3 +85,25 @@ const addCopyButtons = (clipboard) => {
 };
 
 
+const addAnchorLinks = () => {
+  document.querySelectorAll(".content h1, .content h2, .content h3, .content h4").forEach(heading => {
+    let id = heading.innerText.toLowerCase()
+      .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
+      .replace(/ +/g, '-');
+    heading.setAttribute("id", id);
+    heading.classList.add("heading-anchor");
+    let anchor = document.createElement('a');
+    anchor.className = 'anchor-link';
+    anchor.href = '#' + id;
+    anchor.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width=16 height=16 viewBox="0 0 640 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg>';
+    heading.append(anchor);
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault()
+
+      document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start' //scroll to top of the target element
+      })
+    })
+  })
+}
