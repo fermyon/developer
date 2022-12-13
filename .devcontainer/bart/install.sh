a7dc06cbc6264e34229a92781fe45fbe48af92ef
Timothy McCallum <tim.mccallum@fermyon.com>
2022-12-13 14:24:10 +1000

    Update .devcontainer/bart/install.sh
    Signed-off-by: tpmccallum tim.mccallum@fermyon.com
    
    Signed-off-by: Timothy McCallum <tim.mccallum@fermyon.com>

--- .devcontainer/bart/install.sh
+++ .devcontainer/bart/install.sh
@@ -3,7 +3,8 @@
 VERSION="v0.6.0"
 OSARCH="linux-amd64"
 
-wget https://github.com/fermyon/bartholomew/releases/download/${VERSION}/bart
+wget https://github.com/fermyon/bartholomew/releases/download/${VERSION}/bart-cli-${VERSION}-${OSARCH}.tar.gz
+tar -zxvf bart-cli-${VERSION}-${OSARCH}.tar.gz
 su
 mv ./bart /usr/local/bin/
 chmod a+x /usr/local/bin/bart
