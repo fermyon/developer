4968b253c3e77b1bf64c7871c4400736d08b79bf
Timothy McCallum <tim.mccallum@fermyon.com>
2023-02-01 15:36:55 +1000

    Update to non-version-specific installation of Spin
    Signed-off-by: tpmccallum tim.mccallum@fermyon.com
    
    We now know that these Codespaces VMs are actually Ubuntu; so we added the auto-install of dependencies in there also.
    Will need to test this next.
    
    Signed-off-by: Timothy McCallum <tim.mccallum@fermyon.com>

--- .devcontainer/spin/install.sh
+++ .devcontainer/spin/install.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
-VERSION="v0.6.0"
-OSARCH="linux-amd64"
+# VERSION="v0.6.0"
+# OSARCH="linux-amd64"
 
-wget https://github.com/fermyon/spin/releases/download/${VERSION}/spin-${VERSION}-${OSARCH}.tar.gz
-tar xfv spin-${VERSION}-${OSARCH}.tar.gz
+# wget https://github.com/fermyon/spin/releases/download/${VERSION}/spin-${VERSION}-${OSARCH}.tar.gz
+# tar xfv spin-${VERSION}-${OSARCH}.tar.gz
+# su
+# mv ./spin /usr/local/bin/
+
+# New non version-specific approach (using Spin install script)
+curl -fsSL https://developer.fermyon.com/downloads/install.sh | bash
 su
-mv ./spin /usr/local/bin/
+apt-get install -y build-essential libssl-dev pkg-config
+mv spin /usr/local/bin/
+
