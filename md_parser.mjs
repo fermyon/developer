3b3174de0e483c820c29a92f6953fd7991cbb3f9
karthik Ganeshram <karthik.ganeshram@fermyon.com>
2023-03-31 10:33:17 -0400

    fix slugify in search index
    Signed-off-by: karthik Ganeshram <karthik.ganeshram@fermyon.com>

--- md_parser.mjs
+++ md_parser.mjs
@@ -65,7 +65,7 @@ function parseMdFile(file, filepath) {
             documentIndex = {
                 project: file.split("/")[2],
                 title: content.attributes["title"], subheading: subtitle, content: "", keywords: content.attributes["keywords"],
-                url: "/" + file.replace(filepath, "") + "#" + subtitle.toLowerCase().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/g, '')
+                url: "/" + file.replace(filepath, "") + "#" + subtitle.toLowerCase().replace(/[`~!@#$%^&*()_|+=?;:'",.<>\{\}\[\]\\\/]/g, '')
                     .replace(/ +/g, '-')
             }
         }
